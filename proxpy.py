030348a078003994c9c8563e1f445e83f049e76f
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-05-29 23:25:40 -0500

    Add files via upload

--- proxpy.py
+++ proxpy.py
@@ -1,10 +1,10 @@
-import requests
+import requests, random
 
 class dlprox:
     def __init__(self, url):
         self.url = url
 
-    def get(self, filename='proxies.txt'):
+    def get(self, filename='.proxpy-proxies'):
         response = requests.get(self.url)
         if response.status_code == 200:
             with open(filename, 'w') as file:
@@ -13,7 +13,7 @@ class dlprox:
             print("[ProxPy]: Failed to download proxies, Either the URL provided failed to load or your Internet Connection is off.")
 
 class request:
-    def __init__(self, proxy_file='proxies.txt'):
+    def __init__(self, proxy_file='.proxpy-proxies'):
         self.proxy_file = proxy_file
         self.proxies = []
 
@@ -22,10 +22,12 @@ class request:
             self.proxies = file.read().splitlines()
 
     def request_proxy(self):
+        random.shuffle(self.proxies)
         return self.proxies.pop(0)
 
-    def get(self, url, headers=None):
+    def get(self, url, headers=None, proxyInfo=None):
         proxy = self.request_proxy()
+        if proxyInfo == True: print("[ProxPy]: Using proxy "+proxy)
         proxies = {'http': proxy, 'https': proxy}
         try:
             response = requests.get(url, proxies=proxies, headers=None)
@@ -37,8 +39,9 @@ class request:
             print(f"[ProxPy]: GET request failed with error: {e}")
             return None
 
-    def post(self, url, data=None, headers=None):
+    def post(self, url, data=None, headers=None, proxyInfo=None):
         proxy = self.request_proxy()
+        if proxyInfo == True: print("[ProxPy]: Using proxy "+proxy)
         proxies = {'http': proxy, 'https': proxy}
         try:
             response = requests.post(url, proxies=proxies, data=data, headers=headers)
